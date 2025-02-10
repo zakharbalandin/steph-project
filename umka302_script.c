4f514af0873e8ab89c63b2eb101f2232f805a251
Zakhar Balandin <zaharbalandin@yandex.ru>
2025-02-10 17:59:23 +0000

    feat(steph): Init Umka script

---
+++ umka302_script.c
@@ -0,0 +1,32 @@
+// Syntax: MyLogic
+
+#include tracker
+#include <time>
+#include serial
+
+#pragma dynamic 256
+
+main() {
+  for (;;) {
+    new buff[120];
+    new size;
+    new value = 0;
+    if (rsopen() == true) {
+      size = rsrecv(buff, _);
+      for (new i = 0; i < size; i++) {
+        value = (value << 8) | (buff[i] & 0xFF);
+      } 
+      if ((value <= 3000) && (value >= 0)) {
+        printf("Data: ");
+        printf("%d\0", value);
+      }
+      
+      delay(1000);
+      for (new i = 0; i < size; i++) {
+        buff[i] = 0;
+      }
+      rsclose();
+    }
+    delay(1000);
+  }
+}
